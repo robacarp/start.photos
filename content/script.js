e80d730a84a5700984673ba9f201235c063209a6
Robert L Carpenter <robert@robacarp.com>
2018-10-26 15:29:05 -0600

    works -- shows a photo and stuff

--- content/script.js
+++ content/script.js
@@ -2,7 +2,7 @@ async function fetch_image() {
   feed_url = "https://robacarp.github.io/photographic_start/feed.json"
   response = await fetch(feed_url)
   feed = await response.json()
-  number = Math.random() * 100 > 50 ? 1 : 0
+  number = parseInt(Math.random() * 3)
   item = feed.items[number]
 
   set_image(item.url)
@@ -14,22 +14,60 @@ function set_image(url) {
 }
 
 function show_info(item) {
-  css_class = item._meta.bezel_color == 'dark' ? 'dark' : 'light'
 
-  document.body.innerHTML = `
-  <bezel class="${css_class}">
-    <avatar>
-      <img src="${item.author.avatar}">
-    </avatar>
-    <info>
-      <title>${item.content_text}</title>
-      <by-line>
-        By <a href="${item.author.url}">${item.author.name}</a>
-        on <a href="${item._meta.venue}">${item._meta.venue}</a>
-      </by-line>
-    </info>
-  </info>
+  body = document.body
+  css_class = item._meta.bezel_color == 'dark' ? 'dark' : 'light'
+  body.classList.remove('dark', 'light')
+  body.classList.add(css_class)
+
+  bezel = document.querySelector('info')
+  bezel.innerHTML = `
+    <title>${item.content_text}</title>
+    <by-line>
+      By <a href="${item.author.url}">${item.author.name}</a>
+      on <a href="${item._meta.venue_url}">${item._meta.venue}</a>
+    </by-line>
   `
 }
 
+function update_clock(){
+  now = new Date()
+  time = ""
+  sep = ":"
+
+  hour = now.getHours()
+  if (hour > 12) hour -= 12
+  time += hour
+  time += sep
+
+  minute = now.getMinutes()
+  if (minute < 10) minute = "0" + minute
+  time += minute
+  time += sep
+
+  second = now.getSeconds()
+  if (second < 10) second = "0" + second
+  time += second
+
+  document.querySelector('time').innerHTML = time
+
+  date = ""
+  sep = "-"
+
+  year = now.getFullYear()
+  date += year
+  date += sep
+
+  month = now.getMonth()
+  month += 1
+  date += month
+  date += sep
+
+  day = now.getDate()
+  date += day
+
+  document.querySelector('date').innerHTML = date
+}
+
 fetch_image()
+setInterval(update_clock, 200)
