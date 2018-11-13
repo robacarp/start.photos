503ede119810bec8ffef208f0a74d4c4eb78992f
Robert L Carpenter <robert@robacarp.com>
2018-11-13 14:29:08 -0700

    update import script to match new photo dir naming plan

--- assistant/bin/import.rb
+++ assistant/bin/import.rb
@@ -12,7 +12,16 @@ help "could not parse json clipboard" unless parsed
 
 id = parsed["id"]
 output_directory = '/Users/robert/Documents/repositories/robacarp/photographic_start/docs/_data/photos'
-output_file = "#{output_directory}/#{id}.json"
+
+venue = parsed["_meta"]["venue"]["name"]
+parsed["external_url"] =~ %r|^https://500px.com/photo/(\d+)/|
+image_id = $1
+
+puts "Venue: #{venue}"
+puts "Image id: #{image_id}"
+
+new_filename = "#{venue}_#{image_id}.json"
+output_file = "#{output_directory}/#{new_filename}"
 puts "writing to #{output_file}"
 File.open(output_file, "w+") do |f|
   f.puts clipboard
