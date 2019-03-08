80a8b81ad76c2217ac684a8eb8174f3aa71cad6e
Robert L Carpenter <robert@robacarp.com>
2019-03-07 20:52:17 -0700

    update utm url

--- web_extension/content/scripts/new_tab.js
+++ web_extension/content/scripts/new_tab.js
@@ -15,11 +15,6 @@ async function chooseImage() {
 }
 
 async function fillCache(){
-  // for (let cached_image of options.cache.items) {
-  //   let img = Builder.img(cached_image.url)
-  //   document.querySelector('prefetch').appendChild(img)
-  // }
-
   for (let i = options.cache.depth - options.cache.count; i >= 0; i --) {
     let item = await PhotoChooser.pick()
 
@@ -36,7 +31,7 @@ async function fillCache(){
 }
 
 function setImage(url) {
-  let terms_of_service = "utm_source=a_photographic_start&utm_medium=referral"
+  let terms_of_service = "utm_source=a%20photographic%20start&utm_medium=referral"
   let combiner = url.indexOf('?') > 0 ? '&' : '?'
   document.querySelector('background').style.setProperty("background-image", `url(${url}${combiner}${terms_of_service})`)
 }
